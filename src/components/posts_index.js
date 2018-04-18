2993128c5d076f4a01d0d7a41f2a46de403881c6
Alex Orme <orme_alexander_j@lilly.com>
2018-04-18 15:49:14 -0400

    Completed tutorial

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { fetchPost } from '../actions';
+import { fetchPosts } from '../actions';
 import _ from 'lodash';
 
 class PostsIndex extends Component {
   componentDidMount() {
-    this.props.fetchPost();
+    this.props.fetchPosts();
   }
 
   renderPosts() {
@@ -14,7 +14,9 @@ class PostsIndex extends Component {
     return _.map(this.props.posts, post => {
       return(
         <li className="list-group-item" key={post.id}>
-          {post.title}
+          <Link to={`/posts/${post.id}`}>
+            {post.title}
+          </Link>
         </li>
       );
     });
@@ -41,4 +43,4 @@ function mapStateToProps(state) {
   return { posts: state.posts };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsIndex);
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
